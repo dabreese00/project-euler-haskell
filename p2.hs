634ac7649810b8b303f2ec292c3c4173efa49f70
David Breese <dabreese00@gmail.com>
2023-08-16 23:34:51 -0500

    Beautify p2 solution

--- p2.hs
+++ p2.hs
@@ -1,12 +1,11 @@
-evenOrZero :: (Integral a) => a -> a
-evenOrZero x = if x `mod` 2 == 0 then x else 0
-
-fibsum' :: (Integral a) => (a, a, a, a) -> a
-fibsum' (prev, cur, max, sum) = if cur > max then sum
-                                   else fibsum' (cur, prev + cur, max, sum + evenOrZero cur)
-
 fibsum :: (Integral a) => a -> a
 fibsum x = fibsum' (1, 1, x, 0)
+    where fibsum' (prev, cur, max, sum) 
+              | cur > max = sum
+              | otherwise = fibsum' (cur, prev + cur, max, sum + evenOrZero cur)
+              where evenOrZero x 
+                        | x `mod` 2 == 0 = x
+                        | otherwise      = 0
 
 -- Run: fibsum 4000000
 -- Answer: 4613732
